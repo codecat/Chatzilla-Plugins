fe4342996bf608e0bd6daa1292b4284afb434b1e
Angelo Geels <spansjh@gmail.com>
2011-07-29 14:50:02 +0200

    Inline plugins now don't show up in topics. (work in progress.)

--- inline-images/init.js
+++ inline-images/init.js
@@ -6,7 +6,7 @@ plugin.id = "inline-images";
 
 plugin.init = function(glob)
 {
-	plugin.version = "1.0";
+	plugin.version = "1.1";
 	plugin.description = "Turns incoming image links (like imgur) into inline images.";
 	plugin.glob = glob;
 	
@@ -15,19 +15,28 @@ plugin.init = function(glob)
 
 plugin.enable = function()
 {
-	client.munger.addRule("inline-images", new RegExp("(http://([a-z0-9\\-]+\\.)+([a-z]{2,})([A-Za-z0-9\\_\\-/]*)[A-Za-z0-9\\_\\-]{5,}.(jpg|png|gif|bmp))"), function(matchText, containerTag, eventData){
-		var newLink = document.createElementNS(XHTML_NS, "html:a");
-		newLink.setAttribute("href", matchText);
-		newLink.setAttribute("location", "_blank");
-		newLink.setAttribute("style", "text-decoration:none;");
-		
-		var newImage = document.createElementNS(XHTML_NS, "html:img");
-		newImage.setAttribute("src", matchText);
-		newImage.setAttribute("style", "max-width:200px;max-height:200px;");
-		newLink.appendChild(newImage);
-		
-		insertText("", newLink, eventData);
-		containerTag.appendChild(newLink);
+	client.munger.addRule("inline-images", new RegExp("(http://([a-z0-9\\-]+\\.)+([a-z]{2,})([A-Za-z0-9\\_\\-/]*)[A-Za-z0-9\\_\\-]{5,}.(jpg|png|gif|bmp))"), function(matchText, containerTag, eventData, mungerEntry){
+		if(!("dontLogURLs" in eventData)){
+			var newLink = document.createElementNS(XHTML_NS, "html:a");
+			newLink.setAttribute("href", matchText);
+			newLink.setAttribute("location", "_blank");
+			newLink.setAttribute("style", "text-decoration:none;");
+			
+			var newImage = document.createElementNS(XHTML_NS, "html:img");
+			newImage.setAttribute("src", matchText);
+			newImage.setAttribute("style", "max-width:200px;max-height:200px;");
+			newLink.appendChild(newImage);
+			
+			containerTag.appendChild(newLink);
+		}else{
+			//insertLink function doesn't work properly.
+			//TODO: Find a way to do this better
+			var newLink = document.createElementNS(XHTML_NS, "html:a");
+			newLink.setAttribute("href", matchText);
+			newLink.setAttribute("location", "_blank");
+			newLink.appendChild(document.createTextNode(matchText));
+			containerTag.appendChild(newLink);
+		}
 	}, 15, 10);
 	
 	return true;
--- inline-youtube/init.js
+++ inline-youtube/init.js
@@ -6,7 +6,7 @@ plugin.id = "inline-youtube";
 
 plugin.init = function(glob)
 {
-	plugin.version = "1.0";
+	plugin.version = "1.1";
 	plugin.description = "Inline YouTube player for YouTube links.";
 	
 	return "OK";
@@ -15,27 +15,37 @@ plugin.init = function(glob)
 plugin.enable = function()
 {
 	client.munger.addRule("inline-youtube",  /(http:\/\/([a-z]{2,}\.|)youtube.com\/watch\?v=([A-Za-z0-9\-\_]{11}))/, function(matchText, containerTag, eventData){
-		var videoID = matchText.match(/watch\?v=([A-Za-z0-9\-\_]{11})/)[1];
-		
-		var newIframe = document.createElementNS(XHTML_NS, "html:iframe");
-		newIframe.setAttribute("src", "http://www.youtube.com/embed/" + videoID);
-		newIframe.setAttribute("width", "560");
-		newIframe.setAttribute("height", "349");
-		newIframe.setAttribute("frameborder", "0");
-		newIframe.setAttribute("allowfullscreen", "1");
-		
-		insertText("", newIframe, eventData);
-		
-		var newYouTubeLink = document.createElementNS(XHTML_NS, "html:a");
-		newYouTubeLink.setAttribute("href", matchText);
-		newYouTubeLink.appendChild(document.createTextNode(matchText));
-		
-		containerTag.appendChild(document.createElementNS(XHTML_NS, "html:br"));
-		containerTag.appendChild(document.createTextNode("YouTube video: "));
-		containerTag.appendChild(newYouTubeLink);
-		containerTag.appendChild(document.createElementNS(XHTML_NS, "html:br"));
-		containerTag.appendChild(newIframe);
-		containerTag.appendChild(document.createElementNS(XHTML_NS, "html:br"));
+		if(!("dontLogURLs" in eventData)){
+			var videoID = matchText.match(/watch\?v=([A-Za-z0-9\-\_]{11})/)[1];
+			
+			var newIframe = document.createElementNS(XHTML_NS, "html:iframe");
+			newIframe.setAttribute("src", "http://www.youtube.com/embed/" + videoID);
+			newIframe.setAttribute("width", "560");
+			newIframe.setAttribute("height", "349");
+			newIframe.setAttribute("frameborder", "0");
+			newIframe.setAttribute("allowfullscreen", "1");
+			
+			insertText("", newIframe, eventData);
+			
+			var newYouTubeLink = document.createElementNS(XHTML_NS, "html:a");
+			newYouTubeLink.setAttribute("href", matchText);
+			newYouTubeLink.appendChild(document.createTextNode(matchText));
+			
+			containerTag.appendChild(document.createElementNS(XHTML_NS, "html:br"));
+			containerTag.appendChild(document.createTextNode("YouTube video: "));
+			containerTag.appendChild(newYouTubeLink);
+			containerTag.appendChild(document.createElementNS(XHTML_NS, "html:br"));
+			containerTag.appendChild(newIframe);
+			containerTag.appendChild(document.createElementNS(XHTML_NS, "html:br"));
+		}else{
+			//insertLink function doesn't work properly.
+			//TODO: Find a way to do this better
+			var newLink = document.createElementNS(XHTML_NS, "html:a");
+			newLink.setAttribute("href", matchText);
+			newLink.setAttribute("location", "_blank");
+			newLink.appendChild(document.createTextNode(matchText));
+			containerTag.appendChild(newLink);
+		}
 	}, 10, 10);
 	
 	return true;
